78319c8c01aaa77c7cb8cb179477fe5060284ca7
Janelle Cueto <janellecueto@outlook.com>
2019-04-26 10:49:34 -0500

    updated

--- scrolling.js
+++ scrolling.js
@@ -1,79 +1,20 @@
-$('.nav-link').click(function(e){
-    e.preventDefault();
-
-    let startOf = $(this).attr('href').indexOf('#');
-    let clickRef = $(this).attr('href').slice(startOf + 1);
-    let target = $('a[name='+clickRef+']');
-
-});
-
-function mouseIsOver(box){
-    let temp = $(box).parent().find(":hover");
-    return temp.length == 1;
-}
-// $(window).on('wheel', function(e){
-//     alert('scroll detected! delta = '+e.originalEvent.deltaY);
-// })
-$(window).on('wheel', function(e){
-    let welcome = $('#welcome').offset().top;
-    let projects = $('#projects').offset().top;
-    let resume = $('#resume').offset().top;
-    let contact = $('#contact').offset().top;
-
-    let lastScrollTop = $(this).scrollTop();
-    var scrollDirection, targetUp, targetDown, targetEle;
-
-    // $('#scrollDelta').text('ScrollTop: '+lastScrollTop+"delta: "
-    // +e.originalEvent.deltaY+
-    // 'welcom: '+welcome+'projects: '+projects+'resume:'+resume+'contact'+contact);
+//SOURCE: http://jsfiddle.net/dizel3d/1eamwt4e/
+//      for smooth scrolling behavior on nav link clicks
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
     
-    if(e.originalEvent.deltaY > 0){
-        scrollDirection = 'up';
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
     }
-    else if(e.originalEvent.deltaY <= 0){
-        scrollDirection = 'down';
-    }
-
+    
+    // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
-
-    if(lastScrollTop === welcome){
-        targetUp = $('#welcome');
-        targetDown = $('#projects');
-    } else if(lastScrollTop === projects){
-        targetUp = $('#welcome');
-        targetDown = $('#resume');
-    } else if(lastScrollTop === resume){
-        //NOTE: there is a scrolling div inside the resume tab :o
-        if($('#scrolling:hover')){
-            targetUp = null;
-            targetDown = null;
-        } else{
-            targetUp = $('#projects');
-            targetDown = $('#contact');
-        }
-    } else if(lastScrollTop === contact){
-        targetUp = $('#resume');
-        targetDown = $('#contact');
-    } else if (lastScrollTop < projects && lastScrollTop > welcome){
-        targetUp = $('#welcome');
-        targetDown = $("#projects");
-    } else if(lastScrollTop < resume && lastScrollTop > projects){
-        targetUp = $('#projects');
-        targetDown = $('#resume');
-    } else if(lastScrollTop < contact && lastScrollTop > resume){
-        targetUp = $('#resume');
-        targetDown = $('#contact');
-    } else if(lastScrollTop > contact){
-        targetUp = $('#resume');
-        targetDown = $('#contact');
-    }
-
-    if(scrollDirection === 'down'){
-        targetEle = targetDown;
-    } else if(scrollDirection === 'up'){
-        targetEle = targetUp;
-    }
-
-    // scrollThere(targetEle, 400);
-    targetEle[0].scrollIntoView({behavior: "smooth"});
+    
+    // top position relative to the document
+    // $('body, html').animate({scrollTop: pos});   //NOTE: this has been deprecated 
+    $id[0].scrollIntoView({behavior: "smooth"});
 });
+
